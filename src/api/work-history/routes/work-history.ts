c1f06a4cf47cd9eec29d3db7ec6fe21768441fe7
Cristofer <cristoferportela@gmail.com>
2024-02-01 12:36:32 -0300

    work history resources

---
+++ src/api/work-history/routes/work-history.ts
@@ -0,0 +1,7 @@
+/**
+ * work-history router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::work-history.work-history');
---
+++ src/api/work-history/services/work-history.ts
@@ -0,0 +1,7 @@
+/**
+ * work-history service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::work-history.work-history');
