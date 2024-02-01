09aa691156e59141c484460b9fad0ee2db63001a
Cristofer <cristoferportela@gmail.com>
2024-02-01 12:34:44 -0300

    location logged resources created

---
+++ src/api/location-logged/controllers/location-logged.ts
@@ -0,0 +1,7 @@
+/**
+ * location-logged controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::location-logged.location-logged');
---
+++ src/api/location-logged/services/location-logged.ts
@@ -0,0 +1,7 @@
+/**
+ * location-logged service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::location-logged.location-logged');
