6d2d58f4fe6fabf656c1bfb27dc79ad4e3ffc39c
Cristofer <cristoferportela@gmail.com>
2024-02-06 17:10:41 -0300

    removed

--- src/api/schedule/controllers/custom-schedule.ts
+++
@@ -1,8 +0,0 @@
-import {factories} from "@strapi/strapi";
-import {sanitize} from "@strapi/utils";
-
-export default {
-  createSchedule(ctx, next) {
-    return sanitize.contentAPI.output({ok: 'ok'}, strapi.contentType('api::schedule.schedule'))
-  }
-}
